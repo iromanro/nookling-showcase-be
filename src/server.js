49dda9631da72ff1102ef9f582bece9a90ff8079
iromanro <iroirogames@gmail.com>
2020-04-18 14:44:55 -0600

    Fix db

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
 const auth = require('./routes/auth/index.js');
 
 const app = express();

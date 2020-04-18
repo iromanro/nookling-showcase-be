bfc26001e71e9f7c59df27bec06646babb1849bc
iromanro <iroirogames@gmail.com>
2020-04-18 14:45:47 -0600

    Fix db

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,5 +1,4 @@
 // const express = require('express');
-const request = require('request');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const axios = require('axios');

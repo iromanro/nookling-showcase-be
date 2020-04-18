a7f21a7ce6102bc27a43b542d93bca92219ad548
iromanro <iroirogames@gmail.com>
2020-04-18 15:55:27 -0600

    Add db stuff

--- src/server.js
+++ src/server.js
@@ -11,13 +11,13 @@ if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '/Users/topnotch/Desktop/Streaminions/streaminions-app/server/.env' });
 }
 
-const allowedOrigins = ['https://nookling-showcase-fe.herokuapp.com']
+const allowedOrigins = ['https://nookling-showcase-fe.herokuapp.com', 'https://www.nooklingshowcase.com']
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'))
     }
   },
   method: ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE', 'OPTIONS', 'PATCH'],
@@ -42,28 +42,28 @@ app.use((req, res, next) => {
   // }
   //res.append('Access-Control-Allow-Origin', req.headers.origin);
   //res.append('Access-Control-Allow-Credentials', true);
-  res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, UPDATE, OPTIONS, PATCH');
-  res.append('Access-Control-Allow-Headers', 'Authorization, Origin, OK, X-Requested-With, Content-Type, Accept');
-  res.append('Content-Type', 'application/json');
+  res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, UPDATE, OPTIONS, PATCH')
+  res.append('Access-Control-Allow-Headers', 'Authorization, Origin, OK, X-Requested-With, Content-Type, Accept')
+  res.append('Content-Type', 'application/json')
 
   next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({
-  verify: (req, res, buf, encoding) => {
-    //Check is hub signature is in the headers
-    if (req.headers && req.headers['x-hub-signature']) {
-      //Split the signature
-      const xHub = req.headers['x-hub-signature'].split('=');
+// app.use(bodyParser.json({
+//   verify: (req, res, buf, encoding) => {
+//     //Check is hub signature is in the headers
+//     if (req.headers && req.headers['x-hub-signature']) {
+//       //Split the signature
+//       const xHub = req.headers['x-hub-signature'].split('=')
 
-      //Get the hex for the signature from twitch and store it in the req
-      req.twitch_hex = crypto.createHmac(xHub[0], process.env.SUB_SECRET).update(buf).digest('hex');
-      req.twitch_signature = xHub[1];
-    }
-  },
-}));
+//       //Get the hex for the signature from twitch and store it in the req
+//       req.twitch_hex = crypto.createHmac(xHub[0], process.env.SUB_SECRET).update(buf).digest('hex')
+//       req.twitch_signature = xHub[1];
+//     }
+//   },
+// }));
 
 // app.use('/', routes);
 app.use('/api/v1/auth', auth);

411483501ef84953a9a898347cc0d9650ad98f4f
iromanro <iroirogames@gmail.com>
2020-04-18 16:18:17 -0600

    Add db stuff

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 function discordLogin(req, res) {
-  let code = req.body;
+  let code = req.body.code;
   console.log("Code: ", code);
 
   axios({
